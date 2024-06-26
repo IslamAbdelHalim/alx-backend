4b7f2bb3579c71e912c0095bec4b99942805cb11
islam ahmed <eslamelbltagye1111@gmail.com>
2024-06-27 00:03:13 +0300

    a

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Basic dictionary 
+"""
+
+BaseCashing = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCashing):
+    """
+        Basic dictionary 
+    """
+
+    def put(self, key, item):
+        """
+            put data
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            get data
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
